8526222c653ca2e02a618bb0f4e7515057df2821
Pepe García <pepe@pepegar.com>
2024-02-14 13:52:21 +0100

    code from class

--- server.py
+++ server.py
@@ -1,17 +1,36 @@
-from flask import Flask
+import csv
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Exercise 1:
 # Load the CSV in data/ folder a list of dictionaries in the data variable
 data = []
+with open("data/all_stocks_5yr.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        # {"date": "..", "open": ...}
+        data.append(row)
 
 
 # Exercise 2:
 # Render the list of dictionaries in the index
 @app.route("/")
 def index():
-    return "change me"
+
+    filtered_data = []
+    latest_ticker_seen = None
+    # this is similar to a group by in SQL
+    # iterate over data
+    # check if we've already added a row for this company
+    #   add it if we didnt
+    #   skip if we did
+    for row in data:
+        if row["Name"] != latest_ticker_seen:
+            filtered_data.append(row)
+            latest_ticker_seen = row["Name"]
+
+    return render_template("index.html", data=filtered_data)
 
 
 # Exercise 3:
@@ -23,7 +42,30 @@ def index():
 # - a table with all the records
 @app.route("/stock/<stock>")
 def get_stock(stock):
-    return "change me"
+
+    relevant_rows = []
+    all_time_low = float("inf")
+    all_time_high = -float("inf")
+    sum_of_volumes = 0
+
+    for row in data:
+        if row["Name"] == stock:
+            if float(row["low"]) < all_time_low:
+                all_time_low = float(row["low"])
+            if float(row["high"]) > all_time_high:
+                all_time_high = float(row["high"])
+            sum_of_volumes += float(row["volume"])
+            relevant_rows.append(row)
+
+    volume_avg = sum_of_volumes / len(relevant_rows)
+
+    return render_template(
+        "stock.html",
+        volume_avg=volume_avg,
+        all_time_high=all_time_high,
+        all_time_low=all_time_low,
+        data=relevant_rows,
+    )
 
 
-app.run(debug=True)
+app.run(debug=True, port=5555)
