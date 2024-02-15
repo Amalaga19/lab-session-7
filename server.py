d220387433e79d0ca3a3891e1be800a96309a1d3
Amalaga19 <andres.malaga1@gmail.com>
2024-02-15 18:16:23 +0100

    Updated

--- server.py
+++ server.py
@@ -1,37 +1,35 @@
-import csv
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
 # Exercise 1:
 # Load the CSV in data/ folder a list of dictionaries in the data variable
 data = []
-with open("data/all_stocks_5yr.csv") as file:
+
+# Open the file and read the data into the data variable
+with ("data/all_stocks_5yr.csv", r) as file:
     reader = csv.DictReader(file)
     for row in reader:
-        # {"date": "..", "open": ...}
         data.append(row)
+    
+
 
 
 # Exercise 2:
 # Render the list of dictionaries in the index
 @app.route("/")
 def index():
-
+    return render_template("index.html", data=data)
     filtered_data = []
     latest_ticker_seen = None
-    # this is similar to a group by in SQL
-    # iterate over data
-    # check if we've already added a row for this company
-    #   add it if we didnt
-    #   skip if we did
+    # Similar to a group by in SQL
+    #Iterating through the data and only appending the first row of each ticker
     for row in data:
-        if row["Name"] != latest_ticker_seen:
+        if row["ticker"] != latest_ticker_seen:
             filtered_data.append(row)
-            latest_ticker_seen = row["Name"]
-
+            latest_ticker_seen = row["ticker"]
     return render_template("index.html", data=filtered_data)
-
 
 # Exercise 3:
 # Create a new route that accepts the ticker of a stock as a path parameter, and render only their:
@@ -42,30 +40,7 @@ def index():
 # - a table with all the records
 @app.route("/stock/<stock>")
 def get_stock(stock):
-
-    relevant_rows = []
-    all_time_low = float("inf")
-    all_time_high = -float("inf")
-    sum_of_volumes = 0
-
-    for row in data:
-        if row["Name"] == stock:
-            if float(row["low"]) < all_time_low:
-                all_time_low = float(row["low"])
-            if float(row["high"]) > all_time_high:
-                all_time_high = float(row["high"])
-            sum_of_volumes += float(row["volume"])
-            relevant_rows.append(row)
-
-    volume_avg = sum_of_volumes / len(relevant_rows)
-
-    return render_template(
-        "stock.html",
-        volume_avg=volume_avg,
-        all_time_high=all_time_high,
-        all_time_low=all_time_low,
-        data=relevant_rows,
-    )
+    return "change me"
 
 
-app.run(debug=True, port=5555)
+app.run(debug=True)
